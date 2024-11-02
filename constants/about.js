e690f4768d6016a1716c82262f77c3942b2fd670
ngoquyen <reinekaiser@gmail.com>
2024-11-02 10:09:49 +0700

    a

---
+++ constants/about.js
@@ -0,0 +1,57 @@
+export const WHY_CHOOSE = {
+    title: "Cánh Cổng Đưa Bạn Đến Những Hành Trình Khó Quên và Trải Nghiệm Du Lịch Suôn Sẻ",
+    picture: "/img/about/about-img.png",
+    desc: "Khám phá thế giới một cách dễ dàng và đầy cảm hứng cùng Vagabond, người bạn đồng hành lý tưởng cho những chuyến đi đáng nhớ.",
+    reason: [
+        {img: "/icons/luggage-03.svg", label: "Trải nghiệm du lịch tuyệt vời"},
+        {img: "/icons/coin-hand.svg", label: "Giá cả hợp lý, cạnh tranh"},
+        {img: "/icons/compass.svg", label: "Tự do khám phá, tự tin khám phá"}
+    ]
+};
+
+
+export const ABOUT = {
+    text: [
+        {
+            title: "VagaBond là gì?",
+            desc: "VagaBound đem đến cho bạn sự tiện lợi để khám phá các trải nghiệm mới mẻ mọi lúc, mọi nơi. Bạn chỉ cần một cú chạm tay để tận hưởng trọn vẹn."
+        },
+        {
+            title: "Sứ mệnh",
+            desc: "VagaBond là cầu nối đưa bạn tới một thế giới đầy những trải nghiệm và khám phá mới từ trong nước ra tới nước ngoài."
+        },
+        {
+            title: "Cam kết",
+            desc: "VagaBond cam kết sự tiện lợi nhanh chóng, du lịch như 'thổ địa', liên tục săn tìm các trải nghiệm mới mẻ và thú vị. "
+        }
+    ],
+    img: [
+        "/img/about/europe.jpg",
+        "/img/about/fuji.jpg",
+        "/img/about/thailand.jpg"
+    ]
+};
+
+export const TEAM = [
+    {
+        img: "/img/about/member.png",
+        name: "Ngô Phương Quyên",
+        role: "CEO & Founder"
+    },
+    {
+        img: "/img/about/member.png",
+        name: "Phạm Trung Nguyên",
+        role: "CEO & Founder"
+    },
+    {
+        img: "/img/about/member.png",
+        name: "Ngô Thị Như Quỳnh",
+        role: "CEO & Founder"
+    }
+];
+    
+export const ICON_SOCIALS = [
+    "/icons/facebook.svg",
+    "/icons/instagram.svg",
+    "/icons/telegram.svg"
+];
---
+++ constants/contact.js
@@ -0,0 +1,38 @@
+export const BANNER_CONTACT = {
+    img: "/img/contact/banner-2.jpeg",
+    title: "LIÊN HỆ",
+    desc: "Liên hệ với chúng tôi ngay để được hỗ trợ nhanh chóng và giải đáp mọi thắc mắc về chuyến đi của bạn"
+}
+
+export const CONTACT_INFO = [
+    {
+        icon: "/icons/phone.svg",
+        title: "Phone",
+        desc: "Liên hệ với chung tôi thông qua số điện thoại sau đây",
+        info: "+084 1234 567"
+
+    },
+    {
+        icon: "/icons/email.svg",
+        title: "Email",
+        desc: "Liên hệ với chung tôi thông qua địa chỉ email sau đây",
+        info: "Vagabond@gmail.com"
+    },
+    {
+        icon: "/icons/loca.svg",
+        title: "Location",
+        desc: "Liên hệ với chung tôi thông qua địa chỉ sau đây",
+        info: "View on Google Map"
+    },
+]
+
+export const GET_IN_TOUCH = {
+    title: "Liên hệ với chúng tôi một cách dễ dàng",
+    sub_title: "Get In Touch",
+    desc: "Nếu có bất kỳ thắc mắc hay cần hỗ trợ, vui lòng liên hệ với chúng tôi. Chúng tôi luôn sẵn sàng giúp đỡ và cam kết mang đến cho bạn sự hỗ trợ tốt nhất.",
+    user: [
+        "/img/about/member.png",
+        "/img/about/member.png",
+        "/img/about/member.png"
+    ]
+}
