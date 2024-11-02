fe3870bf1f2d93d03bcecfdcc5bd1dd478ce330c
ngoquyen <reinekaiser@gmail.com>
2024-11-02 10:14:19 +0700

    Làm About

---
+++ app/components/About/ProfileCard.js
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import { ICON_SOCIALS } from "@/constants/about";
+import Link from "next/link";
+
+export default function ProfileCard(props) {
+  return (
+    <div className="relative w-[350px] h-[160px] bg-white rounded-2xl shadow-custom duration-500 hover:h-[230px] group">
+      <div className="absolute left-1/2 -top-[50px] -translate-x-2/4 shadow-custom rounded-2xl">
+        <Image
+          className="rounded-2xl group-hover:w-[200px]  transition-all duration-500"
+          src={props.img}
+          width={150}
+          height={150}
+          alt={`${props.name}'s profile`}
+        />
+      </div>
+
+      <div className="text-center flex justify-center items-center h-full">
+        <div className="translate-y-[70px] group-hover:translate-y-[40px] transition-all duration-300">
+          <p className="text-xl p-2">{props.name}</p>
+          
+          {/* Chỉ hiện khi hover */}
+          <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <p className="text-base text-gray-400 pb-2">{props.role}</p>
+            <div className="flex items-center justify-center space-x-3">
+              {ICON_SOCIALS.map((icon, key) => (
+                <Link key={key} href="/" >
+                  <Image src={icon} width={24} height={24} alt="Social icon" />
+                </Link>
+              ))}
+            </div>
+          </div>
+
+          
+        </div>
+      </div>
+    </div>
+  );
+}
